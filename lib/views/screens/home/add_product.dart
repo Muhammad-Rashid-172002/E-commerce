624918a73bcc6b8b9eb4c908b8edb0aa25b81bed
Muhammad Rashid <muhammadrashid172002@gmail.com>
2025-06-04 12:37:01 -0700

    App done using Hive

--- lib/views/screens/home/add_product.dart
+++ lib/views/screens/home/add_product.dart
@@ -16,113 +16,115 @@ class AddProductScreen extends StatelessWidget {
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            SizedBox(height: 50),
-            Card(
-              color: Colors.blue.shade50,
-              elevation: 5,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16),
-              ),
-              margin: EdgeInsets.all(16),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    TextField(
-                      controller: nameController,
-                      decoration: InputDecoration(
-                        labelText: 'Name',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 12),
-                    TextField(
-                      controller: buyRateController,
-                      decoration: InputDecoration(
-                        labelText: 'Buy Rate',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                      ),
-                      keyboardType: TextInputType.number,
-                    ),
-                    SizedBox(height: 12),
-                    TextField(
-                      controller: sellRateController,
-                      decoration: InputDecoration(
-                        labelText: 'Sell Rate',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                      ),
-                      keyboardType: TextInputType.number,
-                    ),
-                    SizedBox(height: 12),
-                    TextField(
-                      controller: qtyController,
-                      decoration: InputDecoration(
-                        labelText: 'Quantity',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                      ),
-                      keyboardType: TextInputType.number,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-
-            SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue, // Button color
-                foregroundColor: Colors.white, // Text color
-                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              SizedBox(height: 50),
+              Card(
+                color: Colors.blue.shade50,
+                elevation: 5,
                 shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
+                  borderRadius: BorderRadius.circular(16),
                 ),
-                elevation: 4,
+                margin: EdgeInsets.all(16),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      TextField(
+                        controller: nameController,
+                        decoration: InputDecoration(
+                          labelText: 'Name',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 12),
+                      TextField(
+                        controller: buyRateController,
+                        decoration: InputDecoration(
+                          labelText: 'Buy Rate',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        keyboardType: TextInputType.number,
+                      ),
+                      SizedBox(height: 12),
+                      TextField(
+                        controller: sellRateController,
+                        decoration: InputDecoration(
+                          labelText: 'Sell Rate',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        keyboardType: TextInputType.number,
+                      ),
+                      SizedBox(height: 12),
+                      TextField(
+                        controller: qtyController,
+                        decoration: InputDecoration(
+                          labelText: 'Quantity',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        keyboardType: TextInputType.number,
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              onPressed: () {
-                final name = nameController.text.trim();
-                final buy =
-                    double.tryParse(buyRateController.text.trim()) ?? 0.0;
-                final sell =
-                    double.tryParse(sellRateController.text.trim()) ?? 0.0;
-                final qty = int.tryParse(qtyController.text.trim()) ?? 0;
 
-                if (name.isNotEmpty && qty > 0) {
-                  final product = Product(
-                    name: name,
-                    buyRate: buy,
-                    sellRate: sell,
-                    quantity: qty,
-                    totalSold: 0,
-                  );
-                  Hive.box<Product>('products').add(product);
-                  Navigator.pop(context);
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('Please enter valid product details'),
-                    ),
-                  );
-                }
-              },
-              child: Text(
-                "Save Product",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue, // Button color
+                  foregroundColor: Colors.white, // Text color
+                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  elevation: 4,
+                ),
+                onPressed: () {
+                  final name = nameController.text.trim();
+                  final buy =
+                      double.tryParse(buyRateController.text.trim()) ?? 0.0;
+                  final sell =
+                      double.tryParse(sellRateController.text.trim()) ?? 0.0;
+                  final qty = int.tryParse(qtyController.text.trim()) ?? 0;
+
+                  if (name.isNotEmpty && qty > 0) {
+                    final product = Product(
+                      name: name,
+                      buyRate: buy,
+                      sellRate: sell,
+                      quantity: qty,
+                      totalSold: 0,
+                    );
+                    Hive.box<Product>('products').add(product);
+                    Navigator.pop(context);
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Please enter valid product details'),
+                      ),
+                    );
+                  }
+                },
+                child: Text(
+                  "Save Product",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/views/screens/home/sell_product.dart
+++ lib/views/screens/home/sell_product.dart
@@ -19,97 +19,102 @@ class _SellProductScreenState extends State<SellProductScreen> {
       appBar: AppBar(title: Text("Sell Product")),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            SizedBox(height: 80),
-            Card(
-              color: Colors.green.shade50, // subtle light green background
-              elevation: 6,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16),
-              ),
-              margin: EdgeInsets.all(16),
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    DropdownButtonFormField<Product>(
-                      decoration: InputDecoration(
-                        labelText: 'Select Product',
-                        labelStyle: TextStyle(color: Colors.green.shade900),
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                      items: products
-                          .map(
-                            (prod) => DropdownMenuItem(
-                              value: prod,
-                              child: Text(prod.name),
-                            ),
-                          )
-                          .toList(),
-                      onChanged: (val) => setState(() => selectedProduct = val),
-                    ),
-                    SizedBox(height: 15),
-                    TextField(
-                      controller: quantityController,
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                        labelText: 'Quantity',
-                        labelStyle: TextStyle(color: Colors.green.shade900),
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-
-            SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green, // Button color
-                foregroundColor: Colors.white, // Text color
-                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 80),
+              Card(
+                color: Colors.green.shade50, // subtle light green background
+                elevation: 6,
                 shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
+                  borderRadius: BorderRadius.circular(16),
                 ),
-                elevation: 4,
+                margin: EdgeInsets.all(16),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      DropdownButtonFormField<Product>(
+                        decoration: InputDecoration(
+                          labelText: 'Select Product',
+                          labelStyle: TextStyle(color: Colors.green.shade900),
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                        items: products
+                            .map(
+                              (prod) => DropdownMenuItem(
+                                value: prod,
+                                child: Text(prod.name),
+                              ),
+                            )
+                            .toList(),
+                        onChanged: (val) =>
+                            setState(() => selectedProduct = val),
+                      ),
+                      SizedBox(height: 15),
+                      TextField(
+                        controller: quantityController,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          labelText: 'Quantity',
+                          labelStyle: TextStyle(color: Colors.green.shade900),
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              onPressed: () {
-                final qty = int.tryParse(quantityController.text.trim()) ?? 0;
-                if (selectedProduct != null &&
-                    qty > 0 &&
-                    selectedProduct!.quantity >= qty) {
-                  selectedProduct!.quantity -= qty;
-                  selectedProduct!.totalSold += qty;
-                  selectedProduct!.save();
-                  ScaffoldMessenger.of(
-                    context,
-                  ).showSnackBar(SnackBar(content: Text('Sale recorded')));
-                  Navigator.pop(context);
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Invalid or insufficient quantity')),
-                  );
-                }
-              },
-              child: Text(
-                "Confirm Sale",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+
+              SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green, // Button color
+                  foregroundColor: Colors.white, // Text color
+                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  elevation: 4,
+                ),
+                onPressed: () {
+                  final qty = int.tryParse(quantityController.text.trim()) ?? 0;
+                  if (selectedProduct != null &&
+                      qty > 0 &&
+                      selectedProduct!.quantity >= qty) {
+                    selectedProduct!.quantity -= qty;
+                    selectedProduct!.totalSold += qty;
+                    selectedProduct!.save();
+                    ScaffoldMessenger.of(
+                      context,
+                    ).showSnackBar(SnackBar(content: Text('Sale recorded')));
+                    Navigator.pop(context);
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Invalid or insufficient quantity'),
+                      ),
+                    );
+                  }
+                },
+                child: Text(
+                  "Confirm Sale",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
